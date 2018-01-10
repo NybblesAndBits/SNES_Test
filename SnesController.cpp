f77bd5fa71795de55bebbab149c77fc9c00a9a02
Andrew Quirin <Quirin.Andrew@Gmail.com>
2018-01-09 22:05:22 -0600

    Tabs -> Spaces.

--- SnesController.cpp
+++ SnesController.cpp
@@ -3,112 +3,109 @@
 
 //=============================================================================
 SnesController::SnesController(uint8_t clock, uint8_t latch, uint8_t data) {
+  // Store the pins.
+  this->clock = clock;
+  this->latch = latch;
+  this->data  = data;
 
-	// Store the pins.
-	this->clock = clock;
-	this->latch = latch;
-	this->data  = data;
+  // Set data direction.
+  pinMode(clock, OUTPUT);
+  pinMode(latch, OUTPUT);
+  pinMode(data, INPUT);
 
-	// Set data direction.
-	pinMode(clock, OUTPUT);
-	pinMode(latch, OUTPUT);
-	pinMode(data, INPUT);
+  // Default output states.
+  digitalWrite(clock, LOW);
+  digitalWrite(latch, LOW);
 
-	// Default output states.
-	digitalWrite(clock, LOW);
-	digitalWrite(latch, LOW);
-
-	// Default internal variable states.
-	state = getRawState();
-	change = 0;
-	pollTime = 0;
-	for (uint8_t i=0; i<BUTTON_COUNT; i++) changeTimes[i] = 0;
+  // Default internal variable states.
+  state = getRawState();
+  change = 0;
+  pollTime = 0;
+  for (uint8_t i=0; i<BUTTON_COUNT; i++) changeTimes[i] = 0;
 }
 
 //=============================================================================
 uint16_t SnesController::getRawState() {
+  uint16_t rawState = 0;
+  uint16_t mask = 0x0800;
 
-	uint16_t rawState = 0;
-	uint16_t mask = 0x0800;
+  // Latch the controller button data.
+  digitalWrite(latch, HIGH);
+  digitalWrite(latch, LOW);
 
-	// Latch the controller button data.
-	digitalWrite(latch, HIGH);
-	digitalWrite(latch, LOW);
+  // Clock in all twelve buttons
+  for (uint8_t i=0; i<BUTTON_COUNT; i++) {
 
-	// Clock in all twelve buttons
-	for (uint8_t i=0; i<BUTTON_COUNT; i++) {
+    // Read in a single bit.
+    if (digitalRead(data)) rawState |= mask;
 
-		// Read in a single bit.
-		if (digitalRead(data)) rawState |= mask;
+    // Shift the mask left
+    mask >>= 1;
 
-		// Shift the mask left
-		mask >>= 1;
+    // Clock
+    digitalWrite(clk, HIGH);
+    digitalWrite(clk, LOW);
+  }
 
-		// Clock
-		digitalWrite(clk, HIGH);
-		digitalWrite(clk, LOW);
-	}
-
-	return rawState;
+  return rawState;
 }
 
 //=============================================================================
 void SnesController::poll() {
+  uint16_t newState = 0;
+  uint16_t mask = 0x0800;
+  change = 0;
 
-	uint16_t newState = 0;
-	uint16_t mask = 0x0800;
-	change = 0;
+  // Latch the controller button data.
+  digitalWrite(latch, HIGH);
+  digitalWrite(latch, LOW);
+  pollTime = mills();
 
-	// Latch the controller button data.
-	digitalWrite(latch, HIGH);
-	digitalWrite(latch, LOW);
-	pollTime = mills();
+  // Clock in all twelve buttons
+  for (uint8_t i=0; i<BUTTON_COUNT; i++) {
 
-	// Clock in all twelve buttons
-	for (uint8_t i=0; i<BUTTON_COUNT; i++) {
+    // Read in a single bit.
+    if (digitalRead(data)) newState |= mask;
 
-		// Read in a single bit.
-		if (digitalRead(data)) newState |= mask;
+    // if newState[bit] != state[bit]
+    if((newState ^ state) & mask) {
+      if(pollTime - changeTime[SNES_CONTROLLER_MASK2INDEX(mask)] > BOUNCE_TIME) {
+        state |= newState & mask;
+        change |= mask;
+        changeTime[SNES_CONTROLLER_MASK2INDEX(mask)] = pollTime;
+      }
+    }
 
-		// if newState[bit] != state[bit]
-		if((newState ^ state) & mask) {
-			if(pollTime - changeTime[SNES_CONTROLLER_MASK2INDEX(mask)] > BOUNCE_TIME) {
-				state |= newState & mask;
-				change |= mask;
-				changeTime[SNES_CONTROLLER_MASK2INDEX(mask)] = pollTime;
-			}
-		}
+    // Shift the mask left
+    mask >>= 1;
 
-		// Shift the mask left
-		mask >>= 1;
-
-		// Clock
-		digitalWrite(clk, HIGH);
-		digitalWrite(clk, LOW);
-	}
+    // Clock
+    digitalWrite(clk, HIGH);
+    digitalWrite(clk, LOW);
+  }
 }
 
 //=============================================================================
 bool SnesController::isPressed(uint16_t buttonMask) {
-	return (state & buttonMask);
+  return (state & buttonMask);
 }
 
 //=============================================================================
 bool SnesController::wasPressed(uint16_t buttonMask) {
-	return (change & buttonMask) && (state & buttonMask);
+  return (change & buttonMask) && (state & buttonMask);
 }
 
 //=============================================================================
 bool SnesController::wasReleased(uint16_t buttonMask) {
-	return (change & buttonMask) && ((~state) & buttonMask);
+  return (change & buttonMask) && ((~state) & buttonMask);
 }
 
 //=============================================================================
 uint32_t getHeldTime(uint16_t buttonMask) {
-	if(SNES_CONTROLLER_MASK2INDEX(buttonMask) < BUTTON_COUNT) {
-		if(state & mask) {
-			return pollTime - changeTime[SNES_CONTROLLER_MASK2INDEX(buttonMask)];
-		}
-	}
-	return 0;
+  if(SNES_CONTROLLER_MASK2INDEX(buttonMask) < BUTTON_COUNT) {
+    if(state & mask) {
+      return pollTime - changeTime[SNES_CONTROLLER_MASK2INDEX(buttonMask)];
+    }
+  }
+  return 0;
 }
--- examples/SnesControllerTest/SnesControllerTest.ino
+++ examples/SnesControllerTest/SnesControllerTest.ino
@@ -16,53 +16,52 @@ uint32_t heldInterval = 500;
 //=============================================================================
 // This function runs once during startup.
 void setup() {
-	Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 //=============================================================================
 // This function repeats forever.
 void loop() {
+  // Poll is called to read the state of the controller.
+  snes.poll();
 
-	// Poll is called to read the state of the controller.
-	snes.poll();
+  // The isPressed() function can be used to check if
+  // a particular button is currently being pressed.
+  // As long as the button is being held down, isPressed()
+  // will always return true.
+  if(snes.isPressed(SnesController::START)) {
+    Serial.println("Start is pressed.");
+  }
 
-	// The isPressed() function can be used to check if
-	// a particular button is currently being pressed.
-	// As long as the button is being held down, isPressed()
-	// will always return true.
-	if(snes.isPressed(SnesController::START)) {
-		Serial.println("Start is pressed.");
-	}
+  // The wasPressed() function can be used to detect
+  // if a button was just pressed. This function will
+  // only return true when a button was not pressed
+  // during the last poll() but now is.
+  if(snes.wasPressed(SnesController::A)) {
+    Serial.println("A was pressed.");
+  }
 
-	// The wasPressed() function can be used to detect
-	// if a button was just pressed. This function will
-	// only return true when a button was not pressed
-	// during the last poll() but now is.
-	if(snes.wasPressed(SnesController::A)) {
-		Serial.println("A was pressed.");
-	}
+  // The wasReleased() function can be used to detect
+  // if a button was just released. This works the
+  // same way as the wasPressed() function but only
+  // returns turn when a button was pressed during the
+  // last poll() but now is not.
+  if(snes.wasReleased(SnesController::A)) {
+    Serial.println("A was released.");
+  }
 
-	// The wasReleased() function can be used to detect
-	// if a button was just released. This works the
-	// same way as the wasPressed() function but only
-	// returns turn when a button was pressed during the
-	// last poll() but now is not.
-	if(snes.wasReleased(SnesController::A)) {
-		Serial.println("A was released.");
-	}
+  // The getHeldTime() function can be used to check
+  // how long a button has been held down in ms.
+  uint32_t nextInterval = heldCount * heldInterval;
+  if(snes.getHeldTime(SnesController::R) > nextInterval) {
+    Serial.print("R has been held for ");
+    Serial.print(nextInterval)
+    Serial.println(" ms.");
+    heldCount++;
+  }
 
-	// The getHeldTime() function can be used to check
-	// how long a button has been held down in ms.
-	uint32_t nextInterval = heldCount * heldInterval;
-	if(snes.getHeldTime(SnesController::R) > nextInterval) {
-		Serial.print("R has been held for ");
-		Serial.print(nextInterval)
-		Serial.println(" ms.");
-		heldCount++;
-	}
-
-	// Reset the held count if R was released.
-	if(snes.wasReleased(SnesController::R)) {
-		heldCount = 0;
-	}
+  // Reset the held count if R was released.
+  if(snes.wasReleased(SnesController::R)) {
+    heldCount = 0;
+  }
 }
